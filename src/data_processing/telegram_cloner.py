f61114e2a3d3c3aadea50d88f981a9de826d6234
Alexeiyaganov <BTLS3@yandex.ru>
2026-01-24 16:26:31 +0300

    260124

--- src/data_processing/telegram_cloner.py
+++ src/data_processing/telegram_cloner.py
@@ -18,7 +18,7 @@ class TelegramStyleCloner:
 
     def __init__(self, config):
         self.config = config
-        self.api_id = config.telegram.api_id
+        self.api_id = config['telegram']['api_id']
         self.api_hash = config.telegram.api_hash
         self.phone = config.telegram.phone
         self.session_name = config.telegram.session_name
